8cb9accaebcb477a17654299354092c4418b8436
Silay! <silayprogramming@gmail.com>
2025-08-12 14:00:09 +0330

    Upload project

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const app = new express()
-const errorHandler = require('./utils/errorHandler');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = new express();
+const errorHandler = require("./utils/errorHandler");
 
-const authRouter = require('./modules/auth/auth.routes');
-const timeEntryRouter = require('./modules/time-entry/timeEntry.routes');
+const authRouter = require("./modules/auth/auth.routes");
+const timeEntryRouter = require("./modules/time-entry/timeEntry.routes");
 
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.get("/", (req, res, next) => {
+  try {
+    return res.status(404).json({
+      success: true,
+      message: "Welcome to ClockIn :)",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-app.use("/auth" , authRouter)
+app.use("/auth", authRouter);
 app.use("/time-entries", timeEntryRouter);
 
+app.use((req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: "Not Found :(",
+  });
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
